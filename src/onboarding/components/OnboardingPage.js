7aa70cea256e6befe07987d3ec25f00b955a604b
skkovi <skkovi14@gmail.com>
2025-06-20 11:39:44 -0400

    init

---
+++ src/onboarding/components/OnboardingPage.js
@@ -0,0 +1,11 @@
+import LoginStep from "./LoginStep";
+export default function OnboardingPage() {
+  return (
+    <div>
+      <h1>New Onboarding Project</h1>
+      <div>
+        <LoginStep />
+      </div>
+    </div>
+  );
+}
